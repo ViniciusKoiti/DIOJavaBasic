c418ad6918fcdc67125a204680b1fb8bc2b7dd8f
Vinicius Koiti <viniciusnakahara@gmail.com>
2023-12-09 19:33:33 -0300

    Alterando classe main

--- src/Banco.java
+++ src/Banco.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
-public class Main {
+public class Banco {
     public static void main(String[] args) {
         ContaBancaria contaBancaria = new ContaBancaria();
         Scanner sc = new Scanner(System.in);
